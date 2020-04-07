be655da98f5cae8e965760ed99d780ac3b4b05bb
Dominik <endrych.d@seznam.cz>
2020-04-07 13:12:52 +0200

    Add John Lemon

---
+++ John Lemon's Haunted Jaunt/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    [SerializeField] float fadeDuration = 1f;
+    [SerializeField] float displayImageDuration = 1f;
+    [SerializeField] GameObject player;
+    [SerializeField] CanvasGroup exitBackgroundImageCanvasGroup;
+    [SerializeField] CanvasGroup caughtBackgroundImageCanvasGroup;
+    [SerializeField] AudioSource exitAudio;
+    [SerializeField] AudioSource caughtAudio;
+    private bool m_IsPlayerAtExit = false;
+    private bool m_IsPlayerCaught = false;
+    private float m_Timer;
+    private bool m_HasAudioPlayed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    private void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
+        }
+    }
+
+    private void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+            
+        }
+    }
+
+}
---
+++ John Lemon's Haunted Jaunt/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float turnSpeed = 20;
+
+    private Vector3 m_Movement;
+    private Quaternion m_Rotation = Quaternion.identity; //proměnná pro uchování rotace
+    private Animator m_Animator;
+    private Rigidbody m_PlayerRb;
+    private AudioSource m_AudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+        m_PlayerRb = gameObject.GetComponent<Rigidbody>();
+        m_AudioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        //přidělení pohybového vektoru
+        m_Movement.Set(horizontalMovement, 0.0f, verticalMovement);
+        m_Movement.Normalize(); //normalizace
+
+        //je hráč v pohybu?
+        bool hasHorizontalInput = !Mathf.Approximately(horizontalMovement, 0.0f);
+        bool hasVerticalInput = !Mathf.Approximately(verticalMovement, 0.0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        m_Animator.SetBool("isWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+        //kam by se měl hráč otočit?
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0.0f);
+
+        m_Rotation.SetLookRotation(desiredForward); //uloží rotaci desiredForward
+
+    }
+
+    //speciální metoda, která umožní změnit, jak se aplikuje Root Motion
+    private void OnAnimatorMove()
+    {
+        m_PlayerRb.MovePosition(m_PlayerRb.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_PlayerRb.MoveRotation(m_Rotation);
+    }
+}
